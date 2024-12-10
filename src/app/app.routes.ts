9b6a065af1e76d31b75aa6c1383d0da1f0b4b269
Fish25041986 <oscar-aguirre@hotmail.es>
2024-12-10 07:23:59 -0500

    Actualización de archivos con últimos cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { CiudadesComponent } from './pages/ciudades/ciudades.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { CreditosComponent } from './pages/creditos/creditos.component';
+import { ReferenciasComponent } from './pages/referencias/referencias.component';
+import { TiposCreditosComponent } from './pages/tipos-creditos/tipos-creditos.component';
+import { InformacionLaboralComponent } from './pages/informacion-laboral/informacion-laboral.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"index",component:IndexComponent},
+    {path:"ciudades",component:CiudadesComponent},
+    {path:"clientes",component:ClientesComponent},
+    {path:"creditos",component:CreditosComponent},
+    {path:"referencias",component:ReferenciasComponent},
+    {path:"tipoCredito",component:TiposCreditosComponent},
+    {path:"informacionLaboral",component:InformacionLaboralComponent},
+];
